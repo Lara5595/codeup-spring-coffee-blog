39c3cef9e3f5e2bc9e75e43ce480504b54187002
David Lara <david.a.lara05@gmail.com>
2022-11-29 12:26:42 -0600

    finish lecture

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function (){
     $(".favorite").on('click', function (){
         let customerId = $(this).attr("data-customer-id");
         let coffeeId = $(this).attr("data-coffee-id");
-        $.get("/coffee/customer" + customerId + "/favorite/" + coffeeId);
+        $.post("/coffee/customer/" + customerId + "/favorite/" + coffeeId);
     })
 })
+
+
+
